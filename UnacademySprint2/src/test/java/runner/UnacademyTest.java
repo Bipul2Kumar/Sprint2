4c5b167a584efbe1ec21a0a4f056112364dc9b51
User <User@DESKTOP-MEL20CT>
2021-05-04 18:14:53 +0530

    Commit

--- UnacademySprint2/src/test/java/runner/UnacademyTest.java
+++ UnacademySprint2/src/test/java/runner/UnacademyTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/login.feature")
+@CucumberOptions(features = "src/test/resources/LoginUnacademy.feature",glue = {"stepDefinition"})
 
 public class UnacademyTest 
 {
